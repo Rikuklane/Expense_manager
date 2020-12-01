f42228bf5cf2e97a8a594ee911fb2c851566dc4e
TriinSchaffrik <schaffrik.triin@gmail.com>
2020-12-02 00:29:12 +0200

    kategooriad

--- ExpManApp.py
+++ ExpManApp.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import*
+from tkinter import *
 from tkinter import font as tkfont
 from tkinter import ttk
 from tkinter import messagebox
@@ -12,82 +12,40 @@ import matplotlib.figure
 import matplotlib.patches
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# Database creation
 try:
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="password"
+        password="xIru4114B8GG9 ."
     )
     EMcursor = mydb.cursor()
     EMcursor.execute("CREATE DATABASE ExpManDatabase")
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
+        print("Something is wrong with your username or password")
     elif err.errno == errorcode.ER_DB_CREATE_EXISTS:
         print("Database already exists")
     else:
         print(err)
 
-# Categories table creation
 try:
     mydbtbl = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="password",
+        password="xIru4114B8GG9 .",
         database="ExpManDatabase"
     )
     EMcursor = mydbtbl.cursor()
-    EMcursor.execute("CREATE TABLE categories (type enum('I', 'E') NOT NULL, name VARCHAR(50) NOT NULL)")
-    # adding preset categories
-    sql = "INSERT INTO categories (type, name) VALUES (%s, %s)"
-    val = [("E", "Food"), ("E", "Travel"), ("E", "Bills"), ("E", "Health"), ("E", "Gifts"), ("E", "Sport"),
-           ("E", "Clothing"), ("E", "Entertainment"), ("E", "Education"), ("E", "Electronics"), ("E", "Other"),
-           ("I", "Salary"), ("I", "Awards"), ("I", "Rental"), ("I", "Investments"), ("I", "Sale"), ("I", "Grants"),
-           ("I", "Dividends"), ("I", "Other")]
-    EMcursor.executemany(sql, val)
-    mydbtbl.commit()
+    EMcursor.execute("CREATE TABLE expenses (year VARCHAR(5) NOT NULL,month VARCHAR(2) NOT NULL, day VARCHAR (2) NOT NULL,"
+                     "aa VARCHAR(255) NOT NULL,catg VARCHAR(255) NOT NULL, descr VARCHAR(200))")
+    EMcursor.execute("CREATE TABLE incomes (year VARCHAR(5) NOT NULL,month VARCHAR(2) NOT NULL, day VARCHAR (2) NOT NULL,"
+                     "aa VARCHAR(255) NOT NULL,catg VARCHAR(255) NOT NULL, descr VARCHAR(200))")
+    EMcursor.execute("CREATE TABLE categories (expense_categ VARCHAR(50), income_categ VARCHAR(50))")
+    EMcursor.execute("INSERT INTO categories VALUES ('Other', 'Other'), ('Salary', 'Food'), ('Family', 'Bill'),"
+                     "('Work', 'Shopping'), ('Scholarchip', 'Transportation')")
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_DB_CREATE_EXISTS:
-        print("Database already exists")
-    else:
-        print(err)
-
-# Expenses table creation
-try:
-    mydbtbl = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="password",
-        database="ExpManDatabase"
-    )
-    EMcursor = mydbtbl.cursor()
-    EMcursor.execute("CREATE TABLE expenses (year VARCHAR(50) NOT NULL,month VARCHAR(50) NOT NULL,"
-                     "day VARCHAR (50) NOT NULL, aa VARCHAR(50) NOT NULL, catg VARCHAR(50) NOT NULL)")
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_DB_CREATE_EXISTS:
-        print("Database already exists")
-    else:
-        print(err)
-
-# Incomes table creation
-try:
-    mydbtbl = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="password",
-        database="ExpManDatabase"
-    )
-    EMcursor = mydbtbl.cursor()
-    EMcursor.execute("CREATE TABLE incomes (year VARCHAR(50) NOT NULL,month VARCHAR(50) NOT NULL,"
-                     " day VARCHAR (50) NOT NULL, aa VARCHAR(50) NOT NULL, catg VARCHAR(50) NOT NULL)")
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
+        print("Something is wrong with your username or password")
     elif err.errno == errorcode.ER_DB_CREATE_EXISTS:
         print("Database already exists")
     else:
@@ -104,6 +62,43 @@ class ExpenseManager(tk.Tk):
         self.title("Expense Manager")
         self.title_font = tkfont.Font(family="Arial", size=18, weight="bold", slant="italic")
 
+        def Add_category():
+            window = Toplevel(self, bg="#2A2A2A")
+            window.geometry("300x200")
+
+            radio_window = tk.Frame(window, bg="#2A2A2A")
+            add_window = tk.Frame(window, bg="#2A2A2A")
+            button_window = tk.Frame(window, bg="#2A2A2A")
+            add_window.place(relx=0.5, rely=0.25, relwidth=0.7, relheight=0.35, anchor="n")
+            radio_window.place(relx=0.5, rely=0.1, relwidth=0.7, relheight=0.15, anchor="n")
+            button_window.place(relx=0.5, rely=0.7, relwidth=0.7, relheight=0.15, anchor="n")
+
+            var = tk.StringVar()
+            radio1 = tk.Radiobutton(radio_window, text="Expenses", variable=var, value="1", indicator=0, bg="#1F1F1F",
+                                    fg="#DED4D4", font=("Arial", 13))
+            radio2 = tk.Radiobutton(radio_window, text="Incomes", variable=var, value="2", indicator=0, bg="#1F1F1F",
+                                    fg="#DED4D4", font=("Arial", 13))
+            label = Label(add_window, text="Category name:", bg="#2A2A2A", fg="#FFFFFF", font=('Arial', 13))
+            category_entry = tk.Entry(add_window, width=20, bg='#FFFFFF', fg='#2A2A2A', font=('Arial', 13))
+
+            def add():
+                category = category_entry.get()
+                var2 = var.get()
+                if var2 == "1":
+                    EMcursor.execute(f"INSERT INTO categories (expense_categ) VALUES ('{category}')")
+                elif var2 == "2":
+                    EMcursor.execute(f"INSERT INTO categories (income_categ) VALUES ('{category}')")
+                mydbtbl.commit()
+                window.destroy()
+
+            add_button = tk.Button(button_window, command=add, text="Add category", width=10, bg='#2A2A2A',
+                                   fg='#FFFFFF', font=('Arial', 13))
+            radio1.pack(side="left", fill="both", expand=True)
+            radio2.pack(side="right", fill="both", expand=True)
+            label.pack(padx=10, pady=10, side="top", anchor="w")
+            category_entry.pack(side="top", anchor="w")
+            add_button.pack(side='top', anchor='center')
+
         # popup messages
         def info():
             messagebox.showwarning("Warning", "We are still working on this")
@@ -112,7 +107,7 @@ class ExpenseManager(tk.Tk):
             answer = messagebox.askyesno("Exit system", "Are you sure you want to quit?")
             if answer:
                 self.quit()
-        
+
         # override the exit button
         self.protocol("WM_DELETE_WINDOW", leave)
 
@@ -128,7 +123,7 @@ class ExpenseManager(tk.Tk):
         menubar.add_cascade(label="File", menu=file_menu)
 
         category_menu = tk.Menu(menubar, tearoff=0, font=("Arial", 11))
-        category_menu.add_command(label="Add category", command=info)
+        category_menu.add_command(label="Add category", command=Add_category)
         category_menu.add_command(label="Remove category", command=info)
         menubar.add_cascade(label="Categories", menu=category_menu)
 
@@ -184,8 +179,7 @@ class StartPage(tk.Frame):
         # taking info from database
         sumlist = []
         for m in ("incomes", "expenses"):
-            sql_select = "select * from %s" % m
-            EMcursor.execute(sql_select)
+            EMcursor.execute(f"select * from {m}")
             records = EMcursor.fetchall()
             a = 0
             for row in records:
@@ -193,15 +187,16 @@ class StartPage(tk.Frame):
                 if today[0] == row[0]:
                     if today[1] == row[1]:
                         a += float(row[3])
-            sumlist = sumlist+[a]
-        sumlist = sumlist+[(sumlist[0]-sumlist[1])]
+            sumlist = sumlist + [a]
+        sumlist = sumlist + [(sumlist[0] - sumlist[1])]
 
         # buttons on start page
-        income_button = tk.Button(button_window1, font=("arial", 13), text="Incomes\n"+str(sumlist[0]), bg="#1F1F1F",
+        income_button = tk.Button(button_window1, font=("arial", 13), text="Incomes\n" + str(sumlist[0]), bg="#1F1F1F",
                                   fg="#DED4D4", command=lambda: controller.show_frame("PageIncome"))
-        expense_button = tk.Button(button_window1, font=("arial", 13), text="Expenses\n"+str(sumlist[1]), bg="#1F1F1F",
+        expense_button = tk.Button(button_window1, font=("arial", 13), text="Expenses\n" + str(sumlist[1]),
+                                   bg="#1F1F1F",
                                    fg="#DED4D4", command=lambda: controller.show_frame("PageExpenses"))
-        balance_button = tk.Button(button_window1, font=("arial", 13), text="Balance\n"+str(sumlist[2]), bg="#1F1F1F",
+        balance_button = tk.Button(button_window1, font=("arial", 13), text="Balance\n" + str(sumlist[2]), bg="#1F1F1F",
                                    fg="#DED4D4")
         income_button.pack(side="left", fill="both", expand=True)
         expense_button.pack(side="left", fill="both", expand=True)
@@ -240,14 +235,11 @@ class PageIncome(tk.Frame):
 
         # SQL
         textbox = ScrolledText(sql_window, wrap=WORD, bg="#2A2A2A", fg="#DED4D4", width=44, height=23)
-        sql_select = "select * from incomes"
-        EMcursor.execute(sql_select)
+        EMcursor.execute("select * from incomes")
         records = EMcursor.fetchall()
         i = 1
-        categ = ["Food", "Bills", "Shopping", "Clothing", "Travel", "Health", "Other"]
         for row in records:
-            text1 = str(i) + ") " + row[2] + "." + row[1] + "." + row[0] + " - " + row[3] + "€ category: " +\
-                    categ[int(row[4])] + "\n"
+            text1 = f'{i}) {row[2]}.{row[1]}.{row[0]} - {row[3]}€ category: {row[4]} note: {row[5]}\n'
             textbox.insert(END, text1)
             textbox.yview(END)
             i += 1
@@ -256,17 +248,23 @@ class PageIncome(tk.Frame):
         # Diagram
         fig = matplotlib.figure.Figure(figsize=(4, 4))
         shape = fig.add_subplot(111)
+        EMcursor.execute("select * from categories")
+        category_records = EMcursor.fetchall()
+        categ = []
+        for row in category_records:
+            categ += [row[0]]
         categories = []
-        for i in range(1, 7):
+        categories_leg = []
+        for i in categ:
+            category1 = 0
             for row in records:
                 if row[4] == str(i):
-                    categories = categories + [row[3]]
-        # making the pie explode
-        explode = []
-        for el in categories:
-            explode.append(0.05)
-        shape.pie(categories, autopct="%1.1f%%", normalize=True, pctdistance=0.78, explode=explode)
-        shape.legend(categ,  bbox_to_anchor=(0., 0.02, 1., .102), loc='upper center',
+                    category1 = category1 + int(row[3])
+            if category1 != 0:
+                categories += [category1]
+                categories_leg += [i]
+        shape.pie(categories, autopct="%1.1f%%", normalize=True)
+        shape.legend(categories_leg, bbox_to_anchor=(0., 0.02, 1., .102), loc='upper center',
                      ncol=2, mode="expand", borderaxespad=0.)
         circle = matplotlib.patches.Circle((0, 0), 0.4, color="#2A2A2A")
         shape.add_artist(circle)
@@ -302,14 +300,12 @@ class PageExpenses(tk.Frame):
 
         # SQL
         textbox = ScrolledText(sql_window, wrap=WORD, bg="#2A2A2A", fg="#DED4D4", width=44, height=23)
-        sql_select = "select * from expenses"
-        EMcursor.execute(sql_select)
+        EMcursor.execute("select * from expenses")
         records = EMcursor.fetchall()
         i = 1
         categ = ["Food", "Bills", "Shopping", "Clothing", "Travel", "Health", "Other"]
         for row in records:
-            text1 = str(i) + ") " + row[2] + "." + row[1] + "." + row[0] + " - " + row[3] + "€ category: " + \
-                    categ[int(row[4])] + "\n"
+            text1 = f'{i}) {row[2]}.{row[1]}.{row[0]} - {row[3]}€ category: {row[4]} note: {row[5]}\n'
             textbox.insert(END, text1)
             textbox.yview(END)
             i += 1
@@ -318,17 +314,23 @@ class PageExpenses(tk.Frame):
         # Diagram
         fig = matplotlib.figure.Figure(figsize=(4, 4))
         shape = fig.add_subplot(111)
+        EMcursor.execute("select * from categories")
+        category_records = EMcursor.fetchall()
+        categ = []
+        for row in category_records:
+            categ += [row[0]]
         categories = []
-        for i in range(1, 7):
+        categories_leg = []
+        for i in categ:
+            category1 = 0
             for row in records:
                 if row[4] == str(i):
-                    categories = categories + [row[3]]
-        # explosion of pie chart
-        explode = []
-        for el in categories:
-            explode.append(0.05)
-        shape.pie(categories, autopct="%1.1f%%", normalize=True, pctdistance=0.78, explode=explode)
-        shape.legend(categ,  bbox_to_anchor=(0., 0.02, 1., .102), loc='upper center',
+                    category1 = category1 + int(row[3])
+            if category1 != 0:
+                categories += [category1]
+                categories_leg += [i]
+        shape.pie(categories, autopct="%1.1f%%", normalize=True)
+        shape.legend(categories_leg, bbox_to_anchor=(0., 0.02, 1., .102), loc='upper center',
                      ncol=2, mode="expand", borderaxespad=0.)
         circle = matplotlib.patches.Circle((0, 0), 0.4, color="#2A2A2A")
         shape.add_artist(circle)
@@ -366,23 +368,54 @@ class EntryPage(tk.Frame):
         money_window.place(relx=0.5, rely=0.7, relwidth=0.7, relheight=0.1, anchor="n")
         add_window.place(relx=0.5, rely=0.85, relwidth=0.25, relheight=0.1, anchor="n")
 
+        def expenses_categories():
+            for category in bothcategories:
+                try:
+                    cat_button.menu.delete(f'{category}')
+                except:
+                    continue
+            categories = []
+            for row in records:
+                categories += [row[1]]
+            categories.reverse()
+            for category in categories:
+                if category != None:
+                    cat_button.menu.add_radiobutton(label=category, variable=var_cat, value=category,
+                                                    background='#2A2A2A',
+                                                    foreground='#FFFFFF', font=('Arial', 12), )
+
+        def incomes_categories():
+            for category in bothcategories:
+                try:
+                    cat_button.menu.delete(f'{category}')
+                except:
+                    continue
+            categories = []
+            for row in records:
+                categories += [row[0]]
+            categories.reverse()
+            for category in categories:
+                if category != None:
+                    cat_button.menu.add_radiobutton(label=category, variable=var_cat, value=category,
+                                                    background='#2A2A2A',
+                                                    foreground='#FFFFFF', font=('Arial', 12), )
+
+        EMcursor.execute("select * from categories")
+        records = EMcursor.fetchall()
+        bothcategories = []
+        for row in records:
+            bothcategories += [row[0]] + [row[1]]
         # different kinds of widgets
         radio1 = tk.Radiobutton(button_window, text="Expenses", variable=var, value="1", indicator=0, bg="#1F1F1F",
-                                fg="#DED4D4", font=("Arial", 13))
+                                fg="#DED4D4", font=("Arial", 13), command=expenses_categories)
         radio2 = tk.Radiobutton(button_window, text="Incomes", variable=var, value="2", indicator=0, bg="#1F1F1F",
-                                fg="#DED4D4", font=("Arial", 13))
+                                fg="#DED4D4", font=("Arial", 13), command=incomes_categories)
         cancel_button = tk.Button(cancel_window, text='<--', command=lambda: controller.show_frame("StartPage"),
                                   bg="#1F1F1F", fg="#DED4D4")
         cat_button = tk.Menubutton(category_window, text="Categories", bg="#1F1F1F", indicator=0,
                                    fg="#DED4D4", font=("Arial", 13), relief="raised")
         cat_button.menu = Menu(cat_button, tearoff=0, bg="#1F1F1F", fg="#DED4D4")
         cat_button["menu"] = cat_button.menu
-        categories = ["Food", "Bills", "Shopping", "Clothing", "Travel", "Health", "Other"]
-        muutuja = 0
-        for category in categories:
-            cat_button.menu.add_radiobutton(label=category, variable=var_cat, value=muutuja, background='#2A2A2A',
-                                            foreground='#FFFFFF', font=('Arial', 12), )
-            muutuja += 1
         desc_label = tk.Label(description_window, text='             Enter Memo              ', bg='#2A2A2A',
                               fg='#DED4D4', font=('Arial', 13))
         description = tk.Entry(description_window, width=20, bg='#FFFFFF', fg='#2A2A2A', font=('Arial', 14))
@@ -407,14 +440,13 @@ class EntryPage(tk.Frame):
             caldate = str(cal.get_date()).split("-")
             sma = str(smma.get())
             category2 = var_cat.get()
+            note = description.get()
             if var2 == "1":
-                expenses_sql = "INSERT INTO expenses VALUES (%s, %s, %s, %s, %s)" % (caldate[0], caldate[1],
-                                                                                     caldate[2], sma, category2)
-                EMcursor.execute(expenses_sql)
+                EMcursor.execute(
+                    f"INSERT INTO expenses VALUES ({caldate[0]}, {caldate[1]}, {caldate[2]}, {sma}, '{category2}', '{note}')")
             elif var2 == "2":
-                incomes_sql = "INSERT INTO incomes VALUES (%s, %s, %s, %s, %s)" % (caldate[0], caldate[1],
-                                                                                   caldate[2], sma, category2)
-                EMcursor.execute(incomes_sql)
+                EMcursor.execute(
+                    f"INSERT INTO incomes VALUES ({caldate[0]}, {caldate[1]}, {caldate[2]}, {sma}, '{category2}', '{note}')")
             mydbtbl.commit()
             controller.show_frame("StartPage")
 
